2c4c232414dce6f6edd2bfc7a2f874b26e1851a5
sahrabullah7 <145543132+sahrabullah7@users.noreply.github.com>
2023-11-28 04:55:05 +0200

    Update testing.py
    forming the class

--- testing.py
+++ testing.py
@@ -2,3 +2,6 @@ from morse import MORSECODE,Morse_function,English_function,choosing_input
 
 # Import the 'unittest' module, which provides a testing framework
 import unittest
+
+# Define a test class thats from 'unittest.TestCase'
+class TestMorseCodeConversion(unittest.TestCase):
