b588894e530e8457a728901fe521b68c296ce196
sahrabullah7 <145543132+sahrabullah7@users.noreply.github.com>
2023-11-28 04:56:17 +0200

    Update testing.py
    first unit test

--- testing.py
+++ testing.py
@@ -5,3 +5,15 @@ import unittest
 
 # Define a test class thats from 'unittest.TestCase'
 class TestMorseCodeConversion(unittest.TestCase):
+
+    # Define a test method for converting English to Morse code
+    def test_english_to_morse(self):
+        # Define the expected Morse code result for the input 'HELLO TKH USERS'
+        expected_result = '.... . .-.. .-.. --- / - -.- .... / ..- ... . .-. ...'
+        
+        # Call the 'Morse_function' function to convert 'HELLO TKH USERS' to Morse code
+        actual_result = Morse_function('HELLO TKH USERS', MORSECODE)
+        
+        # Assert that the actual result matches the expected result; otherwise, raise an AssertionError with a custom error message
+        self.assertEqual(actual_result, expected_result, f"Expected: {expected_result}\nActual: {actual_result}")
+    
